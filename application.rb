df91b1726ca9a56c21f9e9b6411d6d9cb65827d6
Robert Pedersen <rob@worldcrimeleague.org>
2019-05-31 12:38:49 -0400

    done

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World.  My Name is Rob Pedersen"
     resp.finish
   end
 
 end
-
